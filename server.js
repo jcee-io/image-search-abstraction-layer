61e6b8cd226accee6ebfe327e8bd53251a10597c
Justin <jvcruz@ucdavis.edu>
2018-02-11 21:36:12 -0800

    change format for db responses

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', async (req, res) => {
 	const searches = await db.recentSearches();
 
-	res.json(searches);
+	res.json(searches.map(({ query, when }) => ({ query, when })));
 });
 app.get('/:query', async (req,res) => {
 	const page = req.query.offset || 1;
